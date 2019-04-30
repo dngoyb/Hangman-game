8f426b0a43bfa05054e43bda88e76d59ad4b3010
Ngoy Banza <dngoyb@gmail.com>
2019-04-30 09:08:28 +0200

    getGuess function to track number of guesses

--- Hangman.js
+++ Hangman.js
@@ -18,5 +18,23 @@ Hangman.prototype.getPuzzle = function(){
     return puzzle
 }
 
+Hangman.prototype.getGuess = function(guess){
+    guess = guess.toLowerCase()
+    const isUnique = this.guessLetter.includes(guess)
+    const isBadGuess = this.word.includes(guess)
+
+    if (!isUnique){
+        this.guessLetter.push(guess)
+    } 
+    if (!isUnique && !isBadGuess){
+        this.numberOfGuess--
+    }
+}
+
+
 const game = new Hangman('hello', 3)
+game.getGuess('h')
+game.getGuess('l')
 console.log(game.getPuzzle());
+console.log(game.numberOfGuess);
+
